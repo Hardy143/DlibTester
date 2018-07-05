bfb151e7daef84cb05b8926a20d95f76249fac91
Hardy143 <vickihardy88@gmail.com>
2018-07-05 10:47:57 +0100

    Initial Commit

---
+++ DlibTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DlibTester
+//
+//  Created by Vicki Larkin on 05/07/2018.
+//  Copyright © 2018 Vicki Hardy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
